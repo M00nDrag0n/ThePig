c473649599be05f9e1f5cf85c544de50bcecfdaf
Anise Lester and Domninique Youmans <domninique@>
2019-08-28 17:45:07 -0700

    update Javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,65 +1,71 @@
 
-function pigDice(PlayerOne, PlayerTwo, score) {
-  this.PlayerOne = 0;
-  this.PlayerTwo = 0;
-  this.score = 0;
+function PigDice() {
+  var player1 = new Player();
+  var player2 = new Player();
+}
+function Player(name){
+  this.name = name,
+  this.score = score
+}
+Player.prototype.addScore = function(){
 
-  function Player(score){
-    this.score = score
-  }
-  Player.prototype.addScore = function(){
-    this.score +=1;
-  }
+  this.score +=1;
+}
+
+// x = new Player(0);
+// x.addScore();
+
+PigDice.prototype.addScore = function(){
+  this.score +=1;
+  // return this.score;
+}
+
+function pigDice(Player, score) {
+}
+console.log("Function works, right?")
+//    $(document).ready(function(){
+  //
+  //      $(".inputForm").submit(function(event) {
+    //        event.preventDefault();
+    //        console.log("button works");
+    //    });
+    //
+    //
+    // });
 
 
-  PigDice.prototype.addScore = function(){
-    this.score +=1;
-    return this.score;
-  }
-
-  function Player.push();
-
- function pigDice(Player, score) {
-   $("inputForm" + Player).submit(function(event) {
-     event.preventDefault();
-   })
- }
 
 
-
-
-
-
-//
-// }
-//  PlayerOne
-// //
-//
-// function Player = [];
-//   function Player.push();
-//
-//   Math.floor(Math.random()*6) +1 //prevents the program from adding <1 or >6 to the score at a time. "Math.floor" etc causes us to get random numbers. By itself, "Math.floor" tells the CPU to round down from the number that you give it from "Math.random". The first part of it means "less than 6" which is why you have a +1 at the end of the sequence. Without the +1, it only gives you numbers 0-5, rather than 1-6. *6 means less than 6, not less than or equal to 6.
-// //   //
-// //
-// // //Would ".roll" do anything?
-// // //Is there a way to simulate dice using codes or is a link to a dice simulator necessary for this project?
-// // //is there a way to stop a function? (I'm thinking of if the player does not "hold", the score will delete itself and add "0" to the score instead of the total number, possibly by multiplying the "added score" by 0)
-// //
-// //
-// //
-//  PlayerTwo
-// //
-// //
-// //
-// //
-// //
-// //   //
-// //
-// //
-// //
-//  score
-// //
-// //
-// //
-// //
-// //   // j
+    //
+    // }
+    //  PlayerOne
+    // //
+    //
+    // function Player = [];
+    //   function Player.push();
+    //
+    //   Math.floor(Math.random()*6) +1 //prevents the program from adding <1 or >6 to the score at a time. "Math.floor" etc causes us to get random numbers. By itself, "Math.floor" tells the CPU to round down from the number that you give it from "Math.random". The first part of it means "less than 6" which is why you have a +1 at the end of the sequence. Without the +1, it only gives you numbers 0-5, rather than 1-6. *6 means less than 6, not less than or equal to 6.
+    // //   //
+    // //
+    // // //Would ".roll" do anything?
+    // // //Is there a way to simulate dice using codes or is a link to a dice simulator necessary for this project?
+    // // //is there a way to stop a function? (I'm thinking of if the player does not "hold", the score will delete itself and add "0" to the score instead of the total number, possibly by multiplying the "added score" by 0)
+    // //
+    // //
+    // //
+    //  PlayerTwo
+    // //
+    // //
+    // //
+    // //
+    // //
+    // //   //
+    // //
+    // //
+    // //
+    //  score
+    // //
+    // //
+    // //
+    // //
+    // //   // j
